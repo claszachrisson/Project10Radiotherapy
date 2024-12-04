cb1127bb134a55561497b27423a57b70de4df581
Vili1372 <98123778+Vili1372@users.noreply.github.com>
2024-12-04 13:56:37 +0100

    Bugfixes

--- singleSimplex.py
+++ singleSimplex.py
@@ -3,10 +3,10 @@ from LUsolve import LUsolve
 
 def solve(C,A,b,B,Basic,nonBasic,x0):
     c = C.sum(axis=0)
-    Basic = np.concatenate(Basic,range(len(c),len(c)+C.shape[0]))
-    b = np.concatenate(b,C@x0)
+    Basic = np.concatenate((Basic, np.arange(len(c), len(c) + C.shape[0])))
+    b = np.concatenate((b, C @ x0))
     c = np.concatenate((c,np.zeros(C.shape[0])))
-    A = np.hstack(A, np.zeros((A.shape[0],C.shape[0])))
+    A = np.hstack((A, np.zeros((A.shape[0], C.shape[0]))))
     C = np.hstack(C, np.eye(C.shape[0]))
     A = np.vstack(A,C)
 
@@ -20,21 +20,21 @@ def solve(C,A,b,B,Basic,nonBasic,x0):
     for i in range(maxIterations):
         
         b_bar = binv.solve(b)
-        cn = c[nonBasic]-c[Basic]@binv.solve(B[:,nonBasic])
+        cn = c[nonBasic]-c[Basic]@binv.solve(A[:,nonBasic])
 
-        if np.all(cn)<= 1e-5:
+        if np.all(cn <= 1e-5):
             return Basic
         
         s = np.argmax(cn)
         a_s_bar = binv.solve(A[:,s])
         valid = a_s_bar> 0
-        values = np.where(valid, b_bar / a_s_bar[:, s], np.inf)
+        values = np.where(valid, b_bar / a_s_bar, np.inf)
         min_index = np.argmin(values)
         if values[min_index] == np.inf:
             print("Problem is unbounded!")
             return []
 
-        tmp = nonBasic(s)
+        tmp = nonBasic[s]
         nonBasic[s] = Basic[min_index]
         Basic[min_index] = tmp
 
