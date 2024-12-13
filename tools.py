2076df5dbe2c2be027499e938dcf23b72b41f77a
Clas Zachrisson <clas.zachrisson.6871@student.uu.se>
2024-12-13 16:38:58 +0100

    Move LU to tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,22 @@
+from scipy import linalg
+import numpy as np
 import os, sys
+
+class LU:
+    def __init__(self, B):
+        self.B = np.array(B)
+        self.size = self.B.shape[0]
+        self.LU, self.piv = linalg.lu_factor(self.B)
+
+    def solve(self,b):
+        b = np.array(b)
+        if(b.ndim > 1):
+            x = np.zeros([self.size, b.shape[1]])
+            for i in range(b.shape[1]):
+                x[:,i] = linalg.lu_solve((self.LU,self.piv),b[:,i])
+            return x
+        else:
+            return linalg.lu_solve((self.LU,self.piv),b)
 
 class HiddenPrints:
     def __enter__(self):
