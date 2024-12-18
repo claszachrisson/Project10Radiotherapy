8c1bbf4cd27945e40ca63f6092712532068b0908
Clas Zachrisson <clas.zachrisson.6871@student.uu.se>
2024-12-18 22:31:33 +0100

    Add LU2 supporting update once

--- tools.py
+++ tools.py
@@ -18,6 +18,36 @@ class LU:
             return x
         else:
             return linalg.lu_solve((self.LU,self.piv),b)
+    
+class LU2:
+    def __init__(self, B):
+        B = np.array(B)
+        if(B.shape[0] != B.shape[1]):
+            raise ValueError("Matrix not square.")
+        self.size = B.shape[0]
+        self.P, self.L, self.U = linalg.lu(B)
+        self.P2 = np.eye(self.size)
+        self.E = self.P2
+    
+    def solve(self,b):
+        y = linalg.solve_triangular(self.L,self.P.T@b,lower=True)
+        x = linalg.solve_triangular(self.U,self.E@self.P2@y)
+        return self.P2.T@x
+    
+    def update(self,pos,insert):
+        ind = np.concatenate((np.arange(pos), np.arange(pos+1,self.size),[pos]))
+        self.P2 = self.P2[ind]
+        L_Bn = self.U.copy()
+        L_Bn[:,pos] = linalg.solve_triangular(self.L,self.P.T@insert,lower=True)
+        L_Bn = self.P2@L_Bn@self.P2.T
+        spike = L_Bn[-1,:].copy()
+        ls = len(spike)-1
+        self.E = np.eye(ls+1)
+        for i in range(ls):
+            if spike[i] != 0:
+                self.E[ls,i] = -spike[i]/L_Bn[i,i]
+                spike += self.E[ls,i]*L_Bn[i,:]
+        self.U = self.E@L_Bn
         
 class Matrices():
     def __init__(self,A,b,C, B_indb, n_vars):
@@ -41,6 +71,23 @@ class Matrices():
         self.CN_eff = self._CN_eff()
         self.Binvb = self._Binvb()
     
+    # def pivot(self, swap):
+    #     self.B_indb = (self.B_indb & ~(1 << swap[0])) | (1 << swap[1])
+    #     self.N_indb = ~self.B_indb
+    #     print(f"attempting swap {swap} from {self.B_ind}")
+    #     B_out_index = self.B_ind.index(swap[0])
+    #     N_out_index = self.N_ind.index(swap[1])
+    #     self.B_ind[B_out_index] = swap[1]
+    #     self.N_ind[N_out_index] = swap[0]
+    #     self.B[:,B_out_index] = self.A[:,swap[1]]
+    #     self.N[:,N_out_index] = self.A[:,swap[0]]
+    #     self.CB[:,B_out_index] = self.C[:,swap[1]]
+    #     self.CN[:,N_out_index] = self.C[:,swap[0]]
+    #     self.B_inv = LU(self.B)
+    #     self.BinvN[:,N_out_index] = self.B_inv.solve(self.C[:,swap[0]])
+    #     self.CN_eff[:,N_out_index] = self.C[:,swap[0]]-self.CB@self.BinvN[:,N_out_index]
+    #     self.Binvb = self._Binvb()
+
     def _BinvN(self):
         return self.B_inv.solve(self.N)
     
