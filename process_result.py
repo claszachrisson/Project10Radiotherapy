860374ccff027ea8ab17ca2c09d647b17aa1efc2
Vili1372 <98123778+Vili1372@users.noreply.github.com>
2024-12-19 13:15:10 +0100

    results for liver

--- process_result.py
+++ process_result.py
@@ -5,13 +5,21 @@ from mpl_toolkits.mplot3d import Axes3D
 
 A,b,C, i = utils.prob('Liver', True, BDY_downsample=10000, OAR_downsample=1000, PTV_downsample=100)
 
-result = np.load('result_liver_BDY_downsample_{10000}_OAR_downsample_{1000}_PTV_downsample{100}.npz')
+result = np.load(f'result_liver_BDY_downsample_{10000}_OAR_downsample_{1000}_PTV_downsample_{100}.npz')
 
 eff_ind = result['list_data']  
 solution_vec = result['array_data']
 
 cost = C @ solution_vec.T
 
+for i in range(cost.shape[0]):
+    is_strictly_larger = True
+    for j in range(cost.shape[0]):
+        if i != j and not np.all(cost[i] < cost[j]):
+            is_strictly_larger = False
+            break
+    if is_strictly_larger:
+        print("NOO")
 
 
 fig = plt.figure()
@@ -22,3 +30,4 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.legend()
+plt.show()
