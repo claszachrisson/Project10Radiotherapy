53c95de7eb28eb9cd274f21db2b3dedf624238cd
Vili1372 <98123778+Vili1372@users.noreply.github.com>
2024-12-19 12:19:09 +0100

    fixed so a specified amount of solutions can be set

---
+++ process_result.py
@@ -0,0 +1,24 @@
+import numpy as np
+import CORT.utils as utils
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+A,b,C, i = utils.prob('Liver', True, BDY_downsample=10000, OAR_downsample=1000, PTV_downsample=100)
+
+result = np.load('result_liver_BDY_downsample_{10000}_OAR_downsample_{1000}_PTV_downsample{100}.npz')
+
+eff_ind = result['list_data']  
+solution_vec = result['array_data']
+
+cost = C @ solution_vec.T
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(cost[0], cost[1], cost[2], color='r', label='cost point Point')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+ax.legend()
--- run_cort.py
+++ run_cort.py
@@ -3,7 +3,7 @@ import CORT.utils as utils
 
 A,b,C, i = utils.prob('Liver', True, BDY_downsample=10000, OAR_downsample=1000, PTV_downsample=100)
 
-indices, result = simplex(A,b,-C,std_form=True, Initial_basic=i)
+indices, result = simplex(A,b,-C,std_form=True, Initial_basic=i, num_sol = 100)
 
 print(len(indices))
 
